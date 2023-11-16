69d0c9ba4dafe0fb8bb561490b62ed88d5eb5d43
Kryptonomous-B <kryptonomousb@gmail.com>
2023-11-15 21:17:32 -0500

    First Commit

---
+++ HardHat_Signing_Messages/test/verifySignature_Test.ts
@@ -0,0 +1,58 @@
+import { ethers } from 'hardhat';
+import { expect } from 'chai';
+import { SignerWithAddress } from '@nomicfoundation/hardhat-ethers/signers';
+import { VerifySignature } from '../typechain-types';
+
+const { provider, deployContract, getSigners, parseEther, Signature } = ethers;
+
+
+describe('Testing Signature Ethers v6', function(){
+
+let deployer: SignerWithAddress, user1:SignerWithAddress;
+let verifySignature: VerifySignature;
+
+
+before(async function(){
+[deployer, user1] = await getSigners();
+verifySignature = await ethers.deployContract("contracts/VerifySignature.sol:VerifySignature", deployer) as VerifySignature;
+
+});
+
+
+it("Testing Verification", async function(){
+ 
+
+ // Get a message has.  it will return a string.
+ let to = user1.address;
+ let amount = 1;
+ let message = "Testing this message";
+ let nonce = 1;
+
+let messageHash = await verifySignature.getMessageHash(to, amount, message, nonce);
+
+///   messageHash comes back as a STRING needs to be converted to Bytes32 with ethers.getBytes
+let signMessage = await deployer.signMessage(ethers.getBytes(messageHash));
+
+let sig = await Signature.from(signMessage); /// returns sig.v, sig.r, sig.s
+console.log("messageHash: ", messageHash);
+console.log("messageHash Converted:", ethers.getBytes(messageHash));
+console.log("~~~~~~~ Signed message ~~~~~~~~");
+console.log(signMessage);
+console.log("");
+
+console.log("~~~~~~~ using ethers Signature to split sig to v r s ~~~~~~~~");
+console.log("v: ", sig.v);
+console.log("r: ", sig.r);
+console.log("s: ", sig.s);
+
+
+console.log("~~~~~~~ Address matches in Signed Message ~~~~~~~~");
+                                         /// Signer, to, amount, message, nonce, SignedMessage
+console.log(await verifySignature.verify(deployer.address, to, amount, message, nonce, signMessage));
+expect(await verifySignature.verify(deployer.address, to, amount, message, nonce, signMessage)).to.equal(true);
+expect(await verifySignature.verify(deployer.address, to, amount+1, message, nonce, signMessage)).to.equal(false);
+
+
+});
+
+})
